c3320e3f5f648391615af4bab704a09cda5d2218
momin096 <momin09609@gamil.com>
2024-12-03 05:14:49 +0600

    cash out feature added

--- js/cash-out.js
+++ js/cash-out.js
@@ -1,3 +1,19 @@
 
 
-document.getElementById('cash')
+document.getElementById('btn-cash-out').addEventListener('click', function(event){
+    event.preventDefault();
+
+    const balance = getElementValueById('account-balance')
+    const cashOut = getInputValueById('cash-out-amount');
+    const pin = getInputValueById('cash-out-pin');
+
+    if(pin === 1234 && cashOut <= balance){
+        const newBalance = balance - cashOut;
+        console.log('Hey')
+
+        setInnerTextById('account-balance', newBalance);
+    }
+    else{
+        alert('Something Went Wrong');
+    }
+})
